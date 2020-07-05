99bc678ac5c8888e3bd3d6cb34dd37afb00847e6
senmay <57076245+senmay@users.noreply.github.com>
2020-07-05 22:04:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://lit-ridge-91070.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://lit-ridge-91070.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://lit-ridge-91070.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://lit-ridge-91070.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
